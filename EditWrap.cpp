f2f1dd316af2a7e5c2f1be17ff673e2261e364d2
deep-soft <deep-soft@users.noreply.github.com>
2026-04-08 08:15:47 +0300

    Update EditWrap.cpp

--- EditWrap.cpp
+++ EditWrap.cpp
@@ -154,7 +154,7 @@ SHAREDSYMBOL void WINAPI GetPluginInfoW(struct PluginInfo *pi)
 	#else
 		pi->PluginMenuStrings = pszMenu;
 		pi->PluginMenuStringsNumber = 1;
-		pi->Reserved0 = 0x45644664; // EdFd
+		//pi->Reserved = 0x45644664; // EdFd
 	#endif
 }
 
